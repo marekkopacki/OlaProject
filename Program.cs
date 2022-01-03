4e5c64f4723754436b254666d55842b64b8f665a
Marek Kopacki <marek.kopacki@capgemini.com>
2022-01-03 14:03:51 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OlaProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
