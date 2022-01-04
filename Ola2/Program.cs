c8e5d58c0ce7c1e8456a683b68cdb3b7f5dcc717
marekkopacki <m.kopacki85@gmail.com>
2022-01-04 10:01:44 +0100

    ola2 w katalogu ola project w koncu...

---
+++ Ola2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Ola2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Ola!");
+            Console.WriteLine("Dupa blada");
+            Console.WriteLine("kolejna linijka kodu");
+            Console.WriteLine("kolejna linijka kodu");
+            Console.WriteLine("kolejna linijka kodu");
+            Console.WriteLine("kolejna linijka kodu");
+            Console.WriteLine("kolejna linijka kodu");
+        }
+    }
+}
